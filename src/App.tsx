f464446f8ebe1756bdf4333afa459ab29de89abf
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-12-26 21:55:18 -0500

    feat: configure app routing with React Router with bug

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import RecipeDetailPage from "./pages/RecipeDetailPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import SearchResultsPage from "./pages/SearchResultsPage";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/category/:categoryName" element={<CategoryPage />} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetailPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/search" element={<SearchResultsPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+}
